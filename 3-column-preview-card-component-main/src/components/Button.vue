55a6b91ad01548c6765fcba166322143b896e341
imad <imadatyatalah@gmail.com>
2021-05-26 14:15:42 +0100

    feat: Added Button component

---
+++ 3-column-preview-card-component-main/src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.button {
+  font-family: "Lexend Deca", sans-serif;
+  font-size: 15px;
+  cursor: pointer;
+  background-color: var(--very-light-gray);
+  padding: 12px 24px;
+  border: 2px solid white;
+  border-radius: 30px;
+}
+.button:hover {
+  background-color: transparent;
+  color: var(--very-light-gray);
+  border: 2px solid white;
+}
+</style>
