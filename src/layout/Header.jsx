23783cc0bf9fab69d582348b41eed2bf639a4791
imad <imadatyatalah@gmail.com>
2021-06-22 17:46:57 +0100

    Show a humberger menu instead of "toggle"

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -44,7 +44,17 @@ const Header = () => {
         </nav>
 
         <div className={headerStyles.header__toggle}>
-          <button onClick={handleClick}>toggle</button>
+          <button onClick={handleClick}>
+            <svg
+              fill="#bfbfbf"
+              width="25px"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
         </div>
       </header>
     </>
