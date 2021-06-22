4cb463783ef8cf52b4b09d69374ee842ede00049
imad <imadatyatalah@gmail.com>
2021-06-22 19:34:47 +0100

    feat: Added a global layout component

---
+++ src/layout/Global.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Header from "../components/Header";
+
+const Global = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
+
+export default Global;
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,13 @@
+import Global from "../layout/Global";
+
 import "../styles/main.scss";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
-      <Component {...pageProps} />
+      <Global>
+        <Component {...pageProps} />
+      </Global>
     </>
   );
 };
