470c79c0ce3a261bad222eac735637f8752ac2a9
imad <imadatyatalah@gmail.com>
2021-06-24 17:57:36 +0100

    Added default seo config

---
+++ next-seo.config.js
@@ -0,0 +1,8 @@
+export default {
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://url-shortening-api-imadatyatalah.vercel.app/",
+    site_name: "Shortly URL shortening API Challenge",
+  },
+};
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,15 @@
+import { DefaultSeo } from "next-seo";
+
 import Global from "../layout/Global";
+import SEO from "../../next-seo.config";
 
 import "../styles/main.scss";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
+      <DefaultSeo {...SEO} />
+
       <Global>
         <Component {...pageProps} />
       </Global>
