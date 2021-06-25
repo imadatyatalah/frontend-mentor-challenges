d6dd5af645f20b3c836c70793647d986056ee197
imad <imadatyatalah@gmail.com>
2021-06-25 19:52:25 +0100

    Added fallback fonts

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="./favicon-32x32.png"
+            href="/favicon-32x32.png"
           />
 
           <link
