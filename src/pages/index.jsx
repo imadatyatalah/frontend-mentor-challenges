dc6383e9e7fef32a5aa3804a86bbabeecc9b900c
imad <imadatyatalah@gmail.com>
2021-06-25 11:57:28 +0100

    feat: Added hero section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,41 @@
-import homeStyles from "../styles/pages/home.module.scss";
+import React from "react";
+import Image from "next/image";
+
+import { NextSeo } from "next-seo";
+
+import styles from "../styles/pages/home.module.scss";
 
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <NextSeo title="Frontend Mentor | Shortly URL shortening API Challenge" />
+
+      {/* Hero Section */}
+      <section className={styles.container}>
+        <div className={styles.container__img}>
+          <Image
+            src="/illustrations/illustration-working.svg"
+            width="733"
+            height="482"
+            placeholder="blur"
+            blurDataURL="LFKx^@004n-:4on4$R-W:R~C00tm"
+            alt=""
+          />
+        </div>
+
+        <div className={styles.container__info}>
+          <h1>More than just shorter links</h1>
+
+          <p>
+            Build your brand’s recognition and get detailed insights on how your
+            links are performing.
+          </p>
+
+          <button className="primary_btn">Get Started</button>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
