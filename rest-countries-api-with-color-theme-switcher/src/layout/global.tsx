dd08c2c3563137d96a1a3ff52d2ef2f6f9e6efd7
imad <imadatyatalah@gmail.com>
2021-08-08 11:26:04 +0100

    style: Set max-width to 1440px

--- rest-countries-api-with-color-theme-switcher/src/layout/global.tsx
+++ rest-countries-api-with-color-theme-switcher/src/layout/global.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/header";
 const Global = ({ children }: { children: React.ReactNode }) => (
   <>
     <Header />
-    <main className="">{children}</main>
+    <main className="custom_max_width">{children}</main>
   </>
 );
 
