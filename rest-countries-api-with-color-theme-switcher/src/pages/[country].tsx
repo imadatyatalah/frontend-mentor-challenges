a4cce5a02878dbe2e0a8a661239b58fa9c50dc0e
imad <imadatyatalah@gmail.com>
2021-08-08 16:37:49 +0100

    feat: Added details country page

--- rest-countries-api-with-color-theme-switcher/src/pages/[country].tsx
+++ rest-countries-api-with-color-theme-switcher/src/pages/[country].tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
 
@@ -19,13 +21,112 @@ const Country = ({ country }: { country: TCountry }) => {
 
   if (!data) return <div>loading...</div>;
 
+  const {
+    name,
+    flag,
+    demonym,
+    population,
+    subregion,
+    region,
+    capital,
+    nativeName,
+    topLevelDomain,
+    languages,
+    currencies,
+    borders,
+  } = data;
+
   return (
     <>
-      <NextSeo title={data.name} />
+      <NextSeo title={name} />
 
-      <pre>
-        <code>{JSON.stringify(data, null, 2)}</code>
-      </pre>
+      <section className="custom_max-width px-4 sm:px-10 lg:px-12">
+        <button className="bg-white dark:bg-dark-blue my-10 py-2 px-7 rounded-md shadow-custom">
+          <Link href="/">
+            <a className="flex items-center">
+              {/* Arrow left icon */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M10 19l-7-7m0 0l7-7m-7 7h18"
+                />
+              </svg>
+
+              <span className="ml-3">Back</span>
+            </a>
+          </Link>
+        </button>
+
+        <div className="flex">
+          <Image src={flag} width="700" height="500" alt={`${demonym} flag`} />
+        </div>
+
+        <div className="pt-8">
+          <h1 className="text-3xl font-extrabold">{name}</h1>
+
+          <ul className="py-3">
+            <li className="font-light py-px">
+              <span className="font-semibold">Native Name: </span>
+              {nativeName}
+            </li>
+            <li className="font-light py-px">
+              <span className="font-semibold">Population: </span>
+              {population.toLocaleString()}
+            </li>
+            <li className="font-light py-px">
+              <span className="font-semibold">Region: </span>
+              {region}
+            </li>
+            <li className="font-light py-px">
+              <span className="font-semibold">Sub Region: </span>
+              {subregion}
+            </li>
+            <li className="font-light py-px">
+              <span className="font-semibold">Capital: </span>
+              {capital}
+            </li>
+          </ul>
+
+          <ul className="py-3">
+            <li className="font-light py-px">
+              <span className="font-semibold">Top Level Domain: </span>
+              {topLevelDomain}
+            </li>
+            <li className="font-light py-px">
+              <span className="font-semibold">Currencies: </span>
+
+              {currencies?.map((currency) => currency.name)}
+            </li>
+            <li className="font-light py-px">
+              <span className="font-semibold">Languages: </span>
+              {languages?.map((language) => language.name).join(", ")}
+            </li>
+          </ul>
+
+          <div className="py-3">
+            <h2 className="text-xl font-semibold">Border Countries: </h2>
+
+            <div className="my-6">
+              {borders?.map((border) => (
+                <Link href={`/${border}`} key={border}>
+                  <a className="inline-block mb-4 bg-white dark:bg-dark-blue mr-4 py-2 px-7 rounded-md shadow-custom">
+                    {/* TODO: Display country name instead of country alpha 3 code */}
+                    {border}
+                  </a>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
