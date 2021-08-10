af7060b5cf66d6bdcef720518d93c0198baaf40c
imad <imadatyatalah@gmail.com>
2021-08-10 12:54:45 +0100

    feat: Implement search

--- rest-countries-api-with-color-theme-switcher/src/pages/index.tsx
+++ rest-countries-api-with-color-theme-switcher/src/pages/index.tsx
@@ -12,6 +12,7 @@ import CountryCard from "@/components/countryCard";
 const Home = ({ countries }: { countries: Country[] }) => {
   const [page, setPage] = useState(1);
   const [region, setRegion] = useState("");
+  const [searchValue, setSearchValue] = useState("");
 
   const { data } = useSWR<Country[]>(
     "https://restcountries.eu/rest/v2/all",
@@ -27,17 +28,55 @@ const Home = ({ countries }: { countries: Country[] }) => {
       return data.filter((country) => country.region === region);
     }
 
+    if (searchValue) {
+      return data.filter((country) =>
+        country.name.toLowerCase().includes(searchValue.toLowerCase())
+      );
+    }
+
     return data;
-  }, [data, region]);
+  }, [data, region, searchValue]);
 
   const countriesPerPage = 20;
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setSearchValue(e.target.value);
+    setRegion("");
+  };
 
   return (
     <>
       <NextSeo title="Home" />
 
       <section className="">
-        <div className="mx-6 sm:mx-4 md:mx-10 lg:mx-12">
+        <div className="mx-6 flex flex-col sm:mx-4 sm:flex-row sm:justify-between sm:items-center md:mx-10 lg:mx-12">
+          <div className="mt-8 py-1 relative flex items-center w-full sm:w-6/12 lg:w-5/12">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 ml-8 absolute text-gray-400"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+
+            <input
+              type="text"
+              name="country"
+              id="country"
+              onChange={handleInputChange}
+              className="shadow-lg-center py-4 pr-4 pl-16 w-full rounded-md focus:outline-none"
+              placeholder="Search for a country..."
+            />
+          </div>
+
           <FilterByRegion>
             <MenuItem onClick={() => setRegion("Africa")}>Africa</MenuItem>
             <MenuItem onClick={() => setRegion("Americas")}>America</MenuItem>
@@ -59,8 +98,9 @@ const Home = ({ countries }: { countries: Country[] }) => {
 
         <div className="text-center">
           <button
+            className="disabled:cursor-not-allowed"
             onClick={() => setPage(page + 1)}
-            disabled={page * countriesPerPage >= data.length}
+            disabled={page * countriesPerPage >= filteredData.length}
           >
             Load More
           </button>
