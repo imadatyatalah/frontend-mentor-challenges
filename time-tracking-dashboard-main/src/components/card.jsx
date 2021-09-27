baecb0b68639c9d9476b18a6d7c33c0d0d8e7947
imad <imadatyatalah@gmail.com>
2021-09-27 15:33:02 +0100

    refactor: Improve code readability

--- time-tracking-dashboard-main/src/components/card.jsx
+++ time-tracking-dashboard-main/src/components/card.jsx
@@ -1,45 +1,11 @@
 import React from "react";
 
-import WorkIcon from "/icon-work.svg?import.";
-import PlayIcon from "/icon-play.svg?import.";
-import StudyIcon from "/icon-study.svg?import.";
-import ExerciseIcon from "/icon-exercise.svg?import.";
-import SocialIcon from "/icon-social.svg?import.";
-import SelfCareIcon from "/icon-self-care.svg?import.";
-
 const Card = ({ currentTimeframe, data }) => {
   const { title, timeframes } = data;
 
-  let bgColor;
-  let bgIcon;
-  let previousText;
+  const taskClass = `${title.replace(/ /g, "-").toLowerCase()}-task`;
 
-  switch (title) {
-    case "Work":
-      bgColor = "bg-light-red-orange";
-      bgIcon = WorkIcon;
-      break;
-    case "Play":
-      bgColor = "bg-soft-blue";
-      bgIcon = PlayIcon;
-      break;
-    case "Study":
-      bgColor = "bg-light-red";
-      bgIcon = StudyIcon;
-      break;
-    case "Exercise":
-      bgColor = "bg-lime-green";
-      bgIcon = ExerciseIcon;
-      break;
-    case "Social":
-      bgColor = "bg-custom-violet";
-      bgIcon = SocialIcon;
-      break;
-    case "Self Care":
-      bgColor = "bg-soft-orange";
-      bgIcon = SelfCareIcon;
-      break;
-  }
+  let previousText;
 
   switch (currentTimeframe) {
     case "daily":
@@ -55,12 +21,7 @@ const Card = ({ currentTimeframe, data }) => {
 
   return (
     <div
-      className={`${bgColor} my-6 text-white pt-12 rounded-xl sm:my-0`}
-      style={{
-        backgroundImage: `url(${bgIcon})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "top right",
-      }}
+      className={`${taskClass} task my-6 text-white pt-12 rounded-xl sm:my-0`}
     >
       <div className="p-6 bg-dark-blue rounded-xl lg:hover:bg-desaturated-blue lg:hover:cursor-pointer">
         <div className="flex items-center justify-between pb-1">
