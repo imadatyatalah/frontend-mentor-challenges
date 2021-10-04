cac109cc297898297700e3a1f0d4bccd374f968d
imad <imadatyatalah@gmail.com>
2021-10-04 19:58:24 +0100

    feat: Added attribution

--- sunnyside-agency-landing-page-main/src/components/Footer.jsx
+++ sunnyside-agency-landing-page-main/src/components/Footer.jsx
@@ -10,33 +10,51 @@ const LINKS = ["About", "Services", "Projects"];
 const SOCIAL_LINKS = [IconFacebook, IconInstagram, IconTwitter, IconPinterest];
 
 const Footer = () => (
-  <footer className="flex flex-col items-center py-10 bg-[#92d3c5]">
-    <div>
-      <a href="/">
-        <Logo className="fill-[#25564b]" />
-      </a>
-    </div>
-
-    <div className="mt-6 mb-14">
-      <ul className="flex">
-        {LINKS.map((link) => (
-          <li className="mx-4 sm:mx-6" key={link}>
-            <a className="text-Dark-moderate-cyan hover:text-white" href="/#">
-              {link}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
-
-    <div className="flex items-center">
-      {SOCIAL_LINKS.map((SocialLink) => (
-        <a className="mx-3" href="/#" key={SocialLink}>
-          <SocialLink className="fill-[#2C7566] hover:fill-[white]" />
+  <>
+    <footer className="flex flex-col items-center py-10 bg-[#92d3c5]">
+      <div>
+        <a href="/">
+          <Logo className="fill-[#25564b]" />
         </a>
-      ))}
+      </div>
+
+      <div className="mt-6 mb-14">
+        <ul className="flex">
+          {LINKS.map((link) => (
+            <li className="mx-4 sm:mx-6" key={link}>
+              <a className="text-Dark-moderate-cyan hover:text-white" href="/#">
+                {link}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="flex items-center">
+        {SOCIAL_LINKS.map((SocialLink) => (
+          <a className="mx-3" href="/#" key={SocialLink}>
+            <SocialLink className="fill-[#2C7566] hover:fill-[white]" />
+          </a>
+        ))}
+      </div>
+    </footer>
+
+    <div class="attribution">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        href="https://github.com/imadatyatalah"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Imad Atyat-Alah
+      </a>
+      .
     </div>
-  </footer>
+  </>
 );
 
 export default Footer;
