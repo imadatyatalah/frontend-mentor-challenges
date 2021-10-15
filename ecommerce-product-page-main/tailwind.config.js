e175f5f5838c21ce0fb2ba0010368674dc3dbd66
imad <imadatyatalah@gmail.com>
2021-10-15 17:37:35 +0100

    feat: Added tailwindcss & colors & fonts

---
+++ ecommerce-product-page-main/tailwind.config.js
@@ -0,0 +1,26 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: { sans: ["Kumbh Sans", "sans-serif", ...fontFamily.sans] },
+
+      colors: {
+        "color-orange": "var(--color-orange)",
+        "color-pale-orange": "var(--color-pale-orange)",
+
+        "color-dark-blue": "var(--color-dark-blue)",
+        "color-dark-grayish-blue": "var(--color-dark-grayish-blue)",
+        "color-grayish-blue": "var(--color-grayish-blue)",
+        "color-light-grayish-blue": "var(--color-light-grayish-blue)",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
